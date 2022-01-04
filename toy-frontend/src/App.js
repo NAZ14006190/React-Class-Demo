d3ab6db9bc2c75dfb812fdc03e5fdb7700a6d367
NAZ14006190 <naz14006190@btc.ac.uk>
2022-01-04 12:17:51 +0000

    Added products page and component

--- toy-frontend/src/App.js
+++ toy-frontend/src/App.js
@@ -7,6 +7,8 @@ import Footer from './Components/Footer'
 //import screens
 import HomeScreen from './Screens/HomeScreen'
 import ProductScreen from './Screens/ProductScreen'
+import AboutScreen  from './Screens/AboutScreen'
+import ContactScreen from './Screens/ContactScreen'
 
 //import routing for page navigation
 import {
@@ -26,6 +28,8 @@ function App() {
           <Routes>
             <Route exact path="/" element={<HomeScreen/>} />
             <Route exact path ="/products" element={<ProductScreen/>}/>
+            <Route exact path = "/about" element={<AboutScreen/>}/>
+            <Route exact path = "/contact" element={<ContactScreen/>}/>
           </Routes>
         <Footer />
       </Router>
---
+++ toy-frontend/src/data/products.js
@@ -0,0 +1,45 @@
+let products =[
+    {
+        "id":1,
+        "name":"Gassy Gus",
+        "price":3.99,
+        "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas condimentum porttitor nunc, cursus consectetur nisl blandit id.",
+        "image":"img/toy1.PNG"
+    },
+    {
+        "id":2,
+        "name":"Gooey Louie",
+        "price":2.99,
+        "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas condimentum porttitor nunc, cursus consectetur nisl blandit id.",
+        "image":"img/toy2.PNG"
+    },
+    {
+        "id":3,
+        "name":"Guess Who!",
+        "price":5.99,
+        "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas condimentum porttitor nunc, cursus consectetur nisl blandit id.",
+        "image":"img/toy3.PNG"
+    },
+    {
+        "id":4,
+        "name":"Windy Knickers",
+        "price":1.99,
+        "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas condimentum porttitor nunc, cursus consectetur nisl blandit id.",
+        "image":"img/toy4.PNG"
+    },
+    {
+        "id":5,
+        "name":"Family Fortunes",
+        "price":2.99,
+        "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas condimentum porttitor nunc, cursus consectetur nisl blandit id.",
+        "image":"img/toy5.PNG"
+    },
+    {
+        "id":6,
+        "name":"The Floor is Lava",
+        "price":3.99,
+        "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas condimentum porttitor nunc, cursus consectetur nisl blandit id.",
+        "image":"img/toy6.PNG"
+    },
+]
+export default products
